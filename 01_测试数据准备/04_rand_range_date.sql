4a03bbd7d0864c5713c6afea25623f668ebc58bb
710850609 <710850609@qq.com>
2019-09-22 21:20:30 +0800

    增加常用数据库函数

---
+++ 01_测试数据准备/04_rand_range_date.sql
@@ -0,0 +1,180 @@
+-- for mysql
+-- 生成指定时间范围的随机时间
+
+delimiter $$
+create function rand_date(start_time TIMESTAMP, end_time TIMESTAMP) RETURNS TIMESTAMP
+BEGIN
+    DECLARE tmp_year INT(4) DEFAULT 1970;
+    DECLARE tmp_month VARCHAR(2) DEFAULT '01';
+    DECLARE tmp_day VARCHAR(2) DEFAULT '01';
+    DECLARE tmp_hour VARCHAR(2) DEFAULT '00';
+    DECLARE tmp_minute VARCHAR(2) DEFAULT '00';
+    DECLARE tmp_second VARCHAR(2) DEFAULT '00';
+    DECLARE tmp_date_str VARCHAR(14);
+    DECLARE tmp_min_val INT(4);
+    DECLARE tmp_max_val INT(4);
+    DECLARE tmp_rand_val INT(2);
+    -- 是否在最大范围内
+    DECLARE tmp_in_max_range INT(1) DEFAULT 0;
+    DECLARE return_date TIMESTAMP;
+
+    -- 生成随机年份
+    SET tmp_min_val = DATE_FORMAT(start_time, '%Y');
+    SET tmp_max_val = DATE_FORMAT(end_time, '%Y');
+    SET tmp_year = tmp_min_val + CEIL(rand() * (tmp_max_val - tmp_min_val));
+    IF tmp_year = tmp_max_val THEN
+        -- 计算是否达到最大年份
+        SET tmp_in_max_range = 1;
+    END IF;
+
+    -- 生成随机月份
+    IF tmp_in_max_range = 1 THEN
+        -- 如果达到最大年，则最大月份数取最大时间的月份
+        SET tmp_min_val = DATE_FORMAT(start_time, '%m');
+        SET tmp_max_val = DATE_FORMAT(end_time, '%m');
+    ELSE
+        SET tmp_min_val = 1;
+        SET tmp_in_max_range = 0;
+    END IF;
+    SET tmp_rand_val = tmp_min_val + CEIL(rand() * (tmp_max_val - tmp_min_val));
+    IF tmp_rand_val < 10 THEN
+        SET tmp_month = concat('0', tmp_rand_val);
+    ELSE
+        SET tmp_month = concat('', tmp_rand_val);
+    END IF;
+    IF tmp_in_max_range = 1 and tmp_month = tmp_max_val THEN
+        -- 计算是否达到最大年月
+        SET tmp_in_max_range = 1;
+    ELSE
+        SET tmp_in_max_range = 0;
+    END IF;
+		
+    -- 生成随机日期
+    IF tmp_in_max_range = 1 THEN
+        -- 如果达到最大年月，则最大日期数取最大时间的日期
+        SET tmp_min_val = DATE_FORMAT(start_time, '%d');
+        SET tmp_max_val = DATE_FORMAT(end_time, '%d');
+    ELSE
+        SET tmp_min_val = 1;
+        IF tmp_rand_val = 2 THEN
+            SET tmp_max_val = 28;
+        ELSEIF tmp_rand_val = 1 or tmp_rand_val = 3 or tmp_rand_val = 5 or tmp_rand_val = 7 or tmp_rand_val = 8 or tmp_rand_val = 10 or tmp_rand_val = 12 THEN
+            SET tmp_max_val = 31;
+        ELSE
+            SET tmp_max_val = 30;
+        END IF;
+    END IF;
+    SET tmp_rand_val = tmp_min_val + CEIL(rand() * (tmp_max_val - tmp_min_val));
+    IF tmp_rand_val < 10 THEN
+        SET tmp_day = concat('0', tmp_rand_val);
+    ELSE
+        SET tmp_day = concat('', tmp_rand_val);
+    END IF;
+    IF tmp_in_max_range = 1 and tmp_day = tmp_max_val THEN
+        -- 计算是否达到最大年月日
+        SET tmp_in_max_range = 1;
+    ELSE
+        SET tmp_in_max_range = 0;
+    END IF;
+		
+    -- 生成随机小时
+    IF tmp_in_max_range = 1 THEN
+        -- 如果达到最大年月日，则最大小时数取最大时间的小时
+        SET tmp_min_val = DATE_FORMAT(start_time, '%H');
+        SET tmp_max_val = DATE_FORMAT(end_time, '%H');
+    ELSE
+        SET tmp_min_val = 0;
+        SET tmp_max_val = 23;
+    END IF;
+    SET tmp_rand_val = tmp_min_val + CEIL(rand() * (tmp_max_val - tmp_min_val));
+    IF tmp_rand_val < 10 THEN
+        SET tmp_hour = concat('0', tmp_rand_val);
+    ELSE
+        SET tmp_hour = concat('', tmp_rand_val);
+    END IF;
+    IF tmp_in_max_range = 1 and tmp_hour = tmp_max_val THEN
+        -- 计算是否达到最大年月日时
+        SET tmp_in_max_range = 1;
+    ELSE
+        SET tmp_in_max_range = 0;
+    END IF;
+
+-- 生成随机分钟
+    IF tmp_in_max_range = 1 THEN
+        -- 如果达到最大年月日时，则最大分钟数取最大时间的分钟
+        SET tmp_min_val = DATE_FORMAT(start_time, '%i');
+        SET tmp_max_val = DATE_FORMAT(end_time, '%i');
+    ELSE
+        SET tmp_min_val = 0;
+        SET tmp_max_val = 59;
+    END IF;
+    SET tmp_rand_val = tmp_min_val + CEIL(rand() * (tmp_max_val - tmp_min_val));
+    IF tmp_rand_val < 10 THEN
+        SET tmp_minute = concat('0', tmp_rand_val);
+    ELSE
+        SET tmp_minute = concat('', tmp_rand_val);
+    END IF;
+    IF tmp_in_max_range = 1 and tmp_minute = tmp_max_val THEN
+        -- 计算是否达到最大年月日时分
+        SET tmp_in_max_range = 1;
+    ELSE
+        SET tmp_in_max_range = 0;
+    END IF;
+
+    -- 生成随机秒钟
+    IF tmp_in_max_range = 1 THEN
+        -- 如果达到最大年月日时分，则最大秒数取最大时间的秒
+        SET tmp_min_val = DATE_FORMAT(start_time, '%s');
+        SET tmp_max_val = DATE_FORMAT(end_time, '%s');
+    ELSE
+        SET tmp_min_val = 0;
+        SET tmp_max_val = 59;
+    END IF;
+    SET tmp_rand_val = tmp_min_val + CEIL(rand() * (tmp_max_val - tmp_min_val));
+    IF tmp_rand_val < 10 THEN
+        SET tmp_second = concat('0', tmp_rand_val);
+    ELSE
+        SET tmp_second = concat('', tmp_rand_val);
+    END IF;
+
+    SET tmp_date_str = concat(tmp_year, tmp_month, tmp_day, tmp_hour, tmp_minute, tmp_second);
+    SET return_date = str_to_date(tmp_date_str, '%Y%m%d%H%i%s');
+    RETURN return_date;
+END $$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    -- 生成随机秒钟(00-59)
+    SET tmp_second_num = CEIL(rand() * 60);
+    IF tmp_second_num < 10 THEN
+        SET tmp_second = concat('0', tmp_second_num);
+    ELSE
+        SET tmp_second = concat('', tmp_second_num);
+    END IF;
+
+    SET tmp_date_str = concat(tmp_year, tmp_month, tmp_day, tmp_hour, tmp_minute, tmp_second);
+    SET return_date = str_to_date(tmp_date_str, '%Y%m%d%H%i%s');
+    RETURN return_date;
+END $$
