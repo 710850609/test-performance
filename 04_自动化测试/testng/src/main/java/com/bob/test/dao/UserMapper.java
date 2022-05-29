a805f02aa2e45599351a3cd75b05b4e2dfc8640c
lin_bo <710850609@qq.com>
2022-05-29 16:10:48 +0800

    支持junit

--- 04_自动化测试/testng/src/main/java/com/bob/test/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.bob.test.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bob.test.dao.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
